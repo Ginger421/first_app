41ef4be44055b3a10bf1aced5f763300bc474dfc
Virginia Owen <gingerowen0421@gmail.com>
2025-04-04 10:14:02 -0700

    cleaned up code

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,51 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:first_app/text_style.dart';
 
 
-// const startAlignment = Alignment.topLeft;
-// const endAlignment = Alignment.bottomRight;
-
-// class GraidientContainer extends StatelessWidget {
-//   const GraidientContainer({super.key});
-
-//   @override
-//   Widget build(context) {
-//     return Container(
-//       decoration: const BoxDecoration(
-//         gradient: LinearGradient(
-//           colors: [
-//             Color.fromARGB(255, 26, 2, 80),
-//             Color.fromARGB(255, 45, 7, 98),
-//           ],
-//           begin: startAlignment,
-//           end: endAlignment,
-//         ),
-//       ),
-//       child: const Center(
-//         child: StyledText("helllo"),
-//       ),
-//     );
-//   }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class GraidientContainer extends StatelessWidget {
   const GraidientContainer({super.key});
 
--- lib/text_style.dart
+++ lib/text_style.dart
@@ -1,38 +1,5 @@
 import 'package:flutter/material.dart';
 
-// class StyledText extends StatelessWidget {
-//   const StyledText(this.text, {super.key});
-
-//   final String text;
-
-//   @override
-//   Widget build(context) {
-//     return Text(
-//       text,
-//       style: const TextStyle(
-//         color: Colors.white,
-//         fontSize: 28,
-//       ),
-//     );
-//   }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//import 'package:flutter/material.dart';
-
 class MyText extends StatelessWidget {
   
   const MyText(this.text,
